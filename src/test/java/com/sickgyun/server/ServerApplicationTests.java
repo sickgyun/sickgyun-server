4126c6c757b67c68f3590ac822aeb6afd3ae3cfb
Woongbin06 <kob05314@gmail.com>
2023-12-26 09:49:50 +0900

    init

---
+++ src/test/java/com/sickgyun/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sickgyun.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
