0463243c05ecd46fae6cfbd31e9bbcdb8e365b42
bo <jacobhboy66@gmail.com>
2024-01-02 14:21:56 +0900

    refactor(profile): 쿼리 삭제 및 find에서 exist로 변경

--- src/main/java/com/sickgyun/server/profile/domain/repository/ProfileRepository.java
+++ src/main/java/com/sickgyun/server/profile/domain/repository/ProfileRepository.java
@@ -1,15 +1,10 @@
 package com.sickgyun.server.profile.domain.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.sickgyun.server.profile.domain.Profile;
 import com.sickgyun.server.user.domain.User;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-	@Query("select p from Profile p where p.writer = :writer")
-	Optional<Profile> findProfileByWriter(@Param("writer") User writer);
+	boolean existsProfileByWriter(User writer);
 }
