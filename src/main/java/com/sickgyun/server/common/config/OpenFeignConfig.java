6cd3748826e69845d4a020d55fab6ed2476ffef1
bo <jacobhboy66@gmail.com>
2024-01-08 09:01:48 +0900

    chore(config): enable OpenFeign

---
+++ src/main/java/com/sickgyun/server/common/config/OpenFeignConfig.java
@@ -0,0 +1,9 @@
+package com.sickgyun.server.common.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class OpenFeignConfig {
+}
