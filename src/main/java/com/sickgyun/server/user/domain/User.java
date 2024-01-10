121913e6a409cc43d287b65ba8a0b27e21e89697
bo <jacobhboy66@gmail.com>
2024-01-10 14:21:44 +0900

    feat(user): email 추가

--- src/main/java/com/sickgyun/server/user/domain/User.java
+++ src/main/java/com/sickgyun/server/user/domain/User.java
@@ -9,16 +9,23 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
+
+	private String email;
+
+	private Role role;
 
 	@OneToOne(
 		fetch = FetchType.LAZY,
@@ -29,5 +36,15 @@ public class User {
 
 	public void updateProfile(Profile profile) {
 		this.profile = profile;
+	}
+
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public void update(User user) {
+		this.email = user.getEmail();
+		this.name = user.getName();
 	}
 }
