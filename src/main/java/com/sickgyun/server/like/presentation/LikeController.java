57c12088376313ea773cbf8e1f6283845cb6c125
bo <jacobhboy66@gmail.com>
2024-01-10 20:51:20 +0900

    feat(like): getCurrentUser 추가

--- src/main/java/com/sickgyun/server/like/presentation/LikeController.java
+++ src/main/java/com/sickgyun/server/like/presentation/LikeController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sickgyun.server.auth.repository.AuthRepository;
 import com.sickgyun.server.like.service.CommandLikeService;
 import com.sickgyun.server.like.service.QueryLikeService;
-import com.sickgyun.server.user.domain.User;
-import com.sickgyun.server.user.service.UserTempService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,25 +22,22 @@ public class LikeController {
 
 	private final CommandLikeService commandLikeService;
 	private final QueryLikeService queryLikeService;
-	private final UserTempService userTempService;
+	private final AuthRepository authRepository;
 
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/{qna-id}")
 	public void createLike(@PathVariable(name = "qna-id") Long qnAId) {
-		User user = userTempService.getUserId1();
-		commandLikeService.create(qnAId, user);
+		commandLikeService.create(qnAId, authRepository.getCurrentUser());
 	}
 
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{qna-id}")
 	public void deleteLike(@PathVariable(name = "qna-id") Long qnAId) {
-		User user = userTempService.getUserId1();
-		commandLikeService.delete(qnAId, user);
+		commandLikeService.delete(qnAId, authRepository.getCurrentUser());
 	}
 
 	@GetMapping("/{qna-id}")
 	public boolean checkLiked(@PathVariable(name = "qna-id") Long qnAId) {
-		User user = userTempService.getUserId1();
-		return queryLikeService.checkLike(qnAId, user);
+		return queryLikeService.checkLike(qnAId, authRepository.getCurrentUser());
 	}
 }
