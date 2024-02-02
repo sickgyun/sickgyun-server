9105c499a76b3fcce2be2167debec8dd6d5b6e99
이창보 <101192100+jacobhboy@users.noreply.github.com>
2024-02-02 13:05:24 +0900

    cors 해결 (#59)
    * fix(application): REQRUIT_URL을 EVENT_URL로 변경
    
    * fix(application): reqruit을 recruit으로 변경
    
    * fix(recruit): 필요없는 코드 제거
    
    * fix(cors): cors 해결

--- src/main/java/com/sickgyun/server/common/config/InterceptorConfig.java
+++ src/main/java/com/sickgyun/server/common/config/InterceptorConfig.java
@@ -1,6 +1,9 @@
 package com.sickgyun.server.common.config;
 
+import static org.springframework.http.HttpHeaders.*;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,10 +18,32 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
+
+	private static final String ALLOW_ALL_PATH = "/**";
+	private static final String ALLOWED_METHODS = "*";
+	private static final String MAIN_SERVER_DOMAIN = "https://zipgo.pet";
+	private static final String DEV_SERVER_DOMAIN = "https://dev.zipgo.pet";
+	private static final String FRONTEND_LOCALHOST = "http://localhost:3000";
+	private static final String HTTPS_FRONTEND_LOCALHOST = "https://localhost:3000";
+
 	private final JwtParser jwtParser;
 	private final AuthUpdater authUpdater;
 	private final AuthReader authReader;
 	private final UserReader userReader;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping(ALLOW_ALL_PATH)
+			.allowedMethods(ALLOWED_METHODS)
+			.allowedOrigins(
+				MAIN_SERVER_DOMAIN,
+				DEV_SERVER_DOMAIN,
+				FRONTEND_LOCALHOST,
+				HTTPS_FRONTEND_LOCALHOST
+			)
+			.allowCredentials(true)
+			.exposedHeaders(LOCATION, SET_COOKIE);
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
