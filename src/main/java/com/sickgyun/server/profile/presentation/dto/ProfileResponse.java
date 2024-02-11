8b535ea9239bd536e3c8edfd54da0ee12323e6ff
이창보 <101192100+jacobhboy@users.noreply.github.com>
2024-02-11 21:04:40 +0900

    프로필 상세 조회와 프로필 조회에 회사 정보 추가 (#69)
    * refactor(profile): 회사 정보 추가
    
    * refactor(profile): 상세 조회에 admissionYear 추가

--- src/main/java/com/sickgyun/server/profile/presentation/dto/ProfileResponse.java
+++ src/main/java/com/sickgyun/server/profile/presentation/dto/ProfileResponse.java
@@ -6,21 +6,27 @@ import com.sickgyun.server.profile.domain.value.Major;
 public record ProfileResponse(
 	Long userId,
 	String name,
+	Integer admissionYear,
 	String imageUrl,
 	Major major,
 	String githubUrl,
 	String resume,
-	String portfolio
+	String portfolio,
+	Boolean isRecruited,
+	String company
 ) {
 	public static ProfileResponse from(Profile profile) {
 		return new ProfileResponse(
 			profile.getWriter().getId(),
 			profile.getWriter().getName(),
+			profile.getInformation().getAdmissionYear(),
 			profile.getInformation().getImageUrl(),
 			profile.getInformation().getMajor(),
 			profile.getOnlineProfile().getGithubUrl(),
 			profile.getOnlineProfile().getResume(),
-			profile.getOnlineProfile().getPortfolio()
+			profile.getOnlineProfile().getPortfolio(),
+			profile.getCompany().getIsRecruited(),
+			profile.getCompany().getCompany()
 		);
 	}
 }
