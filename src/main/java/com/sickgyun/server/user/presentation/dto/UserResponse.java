d4ece7f77046ccbd69e814cfb7e60d6b339e3b1d
Woongbin06 <kob05314@gmail.com>
2024-03-23 15:36:46 +0900

    refactor(user) : 유저 컬럼 추가 및 리스폰스 변경

--- src/main/java/com/sickgyun/server/user/presentation/dto/UserResponse.java
+++ src/main/java/com/sickgyun/server/user/presentation/dto/UserResponse.java
@@ -1,16 +1,18 @@
 package com.sickgyun.server.user.presentation.dto;
 
 import com.sickgyun.server.user.domain.User;
-import com.sickgyun.server.user.domain.value.Contact;
 
 public record UserResponse(
 	Long id,
 	String name,
 	String email,
-	Contact contact
+	String phoneNumber,
+	Boolean isGraduated,
+	Long cardinal
 ) {
 
 	public static UserResponse from(User user) {
-		return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.getContact());
+		return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.getContact().getPhone(),
+			user.getIsGraduated(), user.getCardinal());
 	}
 }
