f02842412d0a92880427682828f7234f52134d67
bo <jacobhboy66@gmail.com>
2024-04-30 10:22:44 +0900

    fix(article): UpdateRequest 내용 추가

--- main-server/src/main/java/com/sickgyun/server/article/presentation/dto/ArticleUpdateRequest.java
+++ main-server/src/main/java/com/sickgyun/server/article/presentation/dto/ArticleUpdateRequest.java
@@ -3,8 +3,22 @@ package com.sickgyun.server.article.presentation.dto;
 import com.sickgyun.server.article.domain.Article;
 import com.sickgyun.server.user.domain.User;
 
-public record ArticleUpdateRequest() {
+import jakarta.validation.constraints.NotNull;
+
+public record ArticleUpdateRequest(
+	@NotNull(message = "title은 필수 값입니다.")
+	String title,
+	@NotNull(message = "imgUrl은 필수 값입니다.")
+	String imgUrl,
+	@NotNull(message = "url은 필수 값입니다.")
+	String url
+) {
 	public Article toEntity(User currentUser) {
-		return null;
+		return new Article(
+			title,
+			imgUrl,
+			url,
+			currentUser
+		);
 	}
 }
